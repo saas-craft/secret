0f363a074d8bc2c8ca376eade2d0452d750cb981
Steffan Karagianis <steffan.karagianis@gmail.com>
2026-05-17 14:21:07 +1000

    Added Reveal and String functions and updated tests

--- secretstring.go
+++ secretstring.go
@@ -1,3 +1,13 @@
 package secretstring
 
+import "strings"
+
 type SecretString string
+
+func (s SecretString) Reveal() string {
+	return string(s)
+}
+
+func (s SecretString) String() string {
+	return strings.Repeat("*", max(len(s), 1))
+}
--- secretstring_test.go
+++ secretstring_test.go
@@ -1,10 +1,46 @@
 package secretstring
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestString(t *testing.T) {
-	secret := SecretString("my-secret-value")
-	if secret.String() == "my-secret-value" {
-		t.Error("String() must not return the secret value")
+func TestReveal(t *testing.T) {
+	tests := map[string]struct {
+		secret string
+	}{
+		"non-empty secret": {secret: "my-secret-value"},
+		"one character":    {secret: "a"},
+		"empty secret":     {secret: ""},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			secret := SecretString(tc.secret)
+			result := secret.Reveal()
+			if result != tc.secret {
+				t.Error("revealed secret must be the same as the original secret")
+			}
+		})
+	}
+}
+
+func TestStringer(t *testing.T) {
+	tests := map[string]struct {
+		secret string
+	}{
+		"non-empty secret": {secret: "my-secret-value"},
+		"one character":    {secret: "a"},
+		"empty secret":     {secret: ""},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			secret := SecretString(tc.secret)
+			result := fmt.Sprintf("%s", secret)
+			if result == tc.secret {
+				t.Error("formatted string must not be the secret value")
+			}
+		})
 	}
 }
